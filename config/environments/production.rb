8f993ed9e5c73e0fdfba374a5d9aff836d6723ee
Yoshiki Ogiwara <y.ogiwara.1214@gmail.com>
2018-06-01 03:53:30 +0900

    production.rb added "smtp"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => "kangaeru-engine.herokuapp.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['user_name'], #ご自身のgmailアドレス
+    :password => ENV['password'], #ご自身のgmailアドレスのパスワード
+    :authentication => 'login'
+  }
 
 end
