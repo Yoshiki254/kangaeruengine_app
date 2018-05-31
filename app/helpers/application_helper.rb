7b640b56260679d97fcee889422b9ac55691a2a9
Yoshiki Ogiwara <y.ogiwara.1214@gmail.com>
2018-06-01 02:45:19 +0900

    config mailer edit as "local host"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-	 def avatar_url(user)
-    if user.avatar
-      "http://graph.facebook.com/#{user.uid}/picture?type=large" # facebookの画像があった場合
-    else
+	#  def avatar_url(user)
+  #   if user.avatar
+  #     "http://graph.facebook.com/#{user.uid}/picture?type=large" # facebookの画像があった場合
+  #   else
       
-    end
-  end
+  #   end
+  # end
 end
