5c83ba29b1bbd5fde43c44e9537c6b5b577dcc82
Yoshiki Ogiwara <y.ogiwara.1214@gmail.com>
2018-06-01 21:31:17 +0900

    helper facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
-	#  def avatar_url(user)
-  #   if user.avatar
-  #     "http://graph.facebook.com/#{user.uid}/picture?type=large" # facebookの画像があった場合
-  #   else
-      
-  #   end
-  # end
+	 def avatar_url(user)
+    if user.avatar
+      "http://graph.facebook.com/#{user.uid}/picture?type=large" # facebookの画像があった場合
+    else
+    end
+  end
 end
